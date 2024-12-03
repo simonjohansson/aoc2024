1577330c8acf34f5374d285856ca2e3333adc697
Simon Johansson <simon@simonjohansson.com>
2024-12-03 14:00:40 +0100

    Less state

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -7,48 +7,28 @@ sealed class Day3Option {
 }
 
 data class Day3(val input: String) {
-    private fun parse(): List<Day3Option> {
-        val options = mutableListOf<Day3Option>()
-
-        val matches = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)|do\\(\\)|don't\\(\\)".toRegex().findAll(input).toList()
-        for (match in matches) {
-            when (match.groupValues[0]) {
-                "do()" -> options.add(Day3Option.Do())
-                "don't()" -> options.add(Day3Option.Dont())
-                else -> options.add(Day3Option.Mul(match.groupValues[1].toInt(), match.groupValues[2].toInt()))
+    private fun parse() =
+        "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)|do\\(\\)|don't\\(\\)".toRegex()
+            .findAll(input).toList()
+            .map {
+                when (it.groupValues[0]) {
+                    "do()" -> Day3Option.Do()
+                    "don't()" -> Day3Option.Dont()
+                    else -> Day3Option.Mul(it.groupValues[1].toInt(), it.groupValues[2].toInt())
+                }
             }
-        }
-        return options.toList()
-    }
 
     fun part1(): Int {
         return parse()
-            .filter { it is Day3Option.Mul }
-            .map { it as Day3Option.Mul }
-            .map { it.a * it.b }
-            .sum()
+            .filterIsInstance<Day3Option.Mul>()
+            .sumOf { it.a * it.b }
     }
 
-    fun part2(): Int {
-        var shouldMul = true
-        var result = 0
-        for (op in parse()) {
-            when (op) {
-                is Day3Option.Do -> {
-                    shouldMul = true
-                }
-
-                is Day3Option.Dont -> {
-                    shouldMul = false
-                }
-
-                is Day3Option.Mul -> {
-                    if (shouldMul) {
-                        result += op.a * op.b
-                    }
-                }
-            }
+    fun part2() = parse().fold(Pair(0, true), { (acc, shouldMul), op ->
+        when (op) {
+            is Day3Option.Do -> Pair(acc, true)
+            is Day3Option.Dont -> Pair(acc, false)
+            is Day3Option.Mul -> Pair(acc + (op.a * op.b * if (shouldMul) 1 else 0), shouldMul)
         }
-        return result
-    }
+    }).first
 }
