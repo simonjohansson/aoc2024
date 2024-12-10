b3f1f80bb32ec9eeddf8c04b7bad3d2510f2461b
Simon Johansson <simon@simonjohansson.com>
2024-12-10 09:21:31 +0100

    Day 10 part 1 scaffolding

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -3,13 +3,13 @@ package org.example
 data class Day10Input(val nums: List<List<Int>>) {
     companion object {
         fun parse(input: String) = Day10Input(
-            emptyList()
+            input.lines().map { it.toCharArray().map { it.digitToInt() } }
         )
     }
 }
 
 data class Day10(val input: String) {
     fun part1() = Day10Input.parse(input).let { grid ->
-        10
+        -10
     }
 }
--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -6,11 +6,20 @@ class Day10Test {
 
     @Test
     fun parse() {
-        assertEquals("0..111....22222", Day9Input.parse(Reader.Read("day10.example1.txt")).toString())
+        val input = """
+            123
+            456
+            789
+            """.trimIndent()
+        assertEquals(listOf(
+            listOf(1,2,3),
+            listOf(4,5,6),
+            listOf(7,8,9),
+        ), Day10Input.parse(input).nums)
     }
 
     @Test
     fun part1() {
-        assertEquals(36, Day9("2333133121414131402").part1())
+        assertEquals(36, Day10(Reader.Read("day10.example1.txt")).part1())
     }
 }
